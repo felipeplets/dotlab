6e428c3e903e5ed13e907c36b8867c91490ad4b3
Felipe Plets <felipe@plets.me>
2022-11-19 20:20:51 -0800

    chore: updates deps

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,12 @@
-export default {
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
   restoreMocks: true,
   collectCoverage: true,
   coverageDirectory: "coverage",
-  coverageProvider: "v8"
+  coverageProvider: "v8",
+  testEnvironment: "node"
 };
+
+export default config;
